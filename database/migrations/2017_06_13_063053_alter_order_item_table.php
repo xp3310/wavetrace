829dfe1ec79e041569b6dd74eae166604f8a537b
johnny <610121025@ems.ndhu.edu.tw>
2017-06-13 16:45:09 +0800

    order_item 刪掉 check_in check_out, 新增 data column 每日每間房間為一筆 order_item

--- database/migrations/2017_06_13_063053_alter_order_item_table.php
+++ database/migrations/2017_06_13_063053_alter_order_item_table.php
@@ -10,9 +10,13 @@ class AlterOrderItemTable extends Migration {
 		Schema::table('order_item', function(Blueprint $table) {
 			$table->dropColumn('room_id');
 			$table->dropColumn('room_cnt');
+			$table->dropColumn('check_in');
+			$table->dropColumn('check_out');
+
 
 			$table->string('product_id')->after('order_id');
 			$table->integer('product_cnt')->after('product_id');
+			$table->datetime('date')->after('product_cnt');
 		});
 	}
 
