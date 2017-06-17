3c59a7103366aa6f5e588b7d6ff86cd14c8fb1fb
johnny_home <linilnjohnny2@gmail.com>
2017-06-18 03:56:06 +0800

    補檔案

---
+++ app/Http/Controllers/SysConfigController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use App\User;
+use App\Http\Controllers\Controller;
+use App\SysConfig;
+use Storage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+
+
+class SysConfigController extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+
+    public function destroy(Request $request, $id) {
+
+    }
+
+    public function edit($id) {
+
+    }
+
+    public function updateAll(Request $request) {
+        $request = $request->all();
+
+        foreach ($request as $k => $v) {
+            if ($k == 'contactInfo') {
+                $v = json_encode($v);
+            }
+            if ($k == 'socialInfo') {
+                $v = json_encode($v);
+            }
+
+            SysConfig::updateOrCreate(['name' => $k],
+                                      ['name' => $k, 'value' => $v]);
+        }
+        echo json_encode( ['status' => 'true', 'msg' => 'ok', 'extInfo' => []]);
+    }
+
+
+}
