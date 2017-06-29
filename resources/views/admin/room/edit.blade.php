97c696bb9664388765cf26f76813ca236cce08a0
June Hsieh <pmkw52525@gmail.com>
2017-06-29 18:23:09 +0800

    微修改帶回家寫

--- resources/views/admin/room/edit.blade.php
+++ resources/views/admin/room/edit.blade.php
@@ -1,13 +1,57 @@
+<?php
+    $myFormLib = new App\Mylib\MyForm('form');
+?>
+
 @extends('admin.layouts.main')
 
 @section('content')
 
 <div class='content'>
 	<div class="content-header">
-    	<h3><b>{{ $room ? $product->title : '新增房型' }}</b></h3>
+    	<h3><b>{{ isset($room) ? $product->title : __('room.create') }}</b></h3>
 	</div>
 	<div class="content-body">
+	    <el-card class="box-card">
+	        <div slot="header" class="clearfix">
+	            <span>@lang('admin.siteInfo')</span>
+	        </div>
+		{!! $myFormLib->elFormOpen() !!}
+
+            {!! $myFormLib->getElItem( 'text', ['name' => 'title', 			'label' => trans('room.title')		 ] ) !!}
+            {!! $myFormLib->getElItem( 'text', ['name' => 'description', 	'label' => trans('admin.introduction')] ) !!}
+			{!! $myFormLib->getElItem( 'buttons', ['label' => '', 'buttons' => ['submit' => ['buttonTittle' => trans('common.save'), 'buttonType' => 'primary', 'callback' => 'onMyFormSubmit'],
+                                                                                'cancal' => ['buttonTittle' => trans('common.cancel'), 'callback' => 'onMyFormCancel'] ] ]  ) !!}
+        {!! $myFormLib->elFormClose() !!}
+	    </el-card>
 	</div>
 </div>
 @endsection
 
+
+@section('vueCustomParam')
+<script>
+    var customVueData = {
+        form: {},
+        loading: false,
+    },
+    customVueMethod = {
+        onMyFormSubmit() {
+
+        },
+
+        onMyFormCancel() {
+            window.location.reload();
+        },
+
+
+        onAddcontactInfo() {
+            this.form.contactInfo.push( {'label': '', 'value': ''} )
+        }
+    };
+
+
+    bnb.vue.setData(customVueData);
+    bnb.vue.setMethod(customVueMethod);
+
+</script>
+@endsection
